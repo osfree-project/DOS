bc9e4b4af79ef3ec7b10af2449b94fda51bd13af
prokushev <yuri.prokushev@gmail.com>
2023-01-28 22:39:49 +0700

    MOVE port added

---
+++ CMD/move/move.mk
@@ -0,0 +1,8 @@
+# $Id: makefile,v 1.1 2004/08/16 06:27:05 prokushev Exp $
+
+PORT_NAME = move
+PORT_TYPE = git
+PORT_URL  = https://github.com/FDOS/move
+PORT_REV  = 
+
+!include $(%ROOT)tools/mk/port.mk
