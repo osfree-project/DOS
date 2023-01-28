051187428c60773d39aac078dbc1be06fb862443
prokushev <yuri.prokushev@gmail.com>
2023-01-28 21:11:10 +0700

    diskcomp, find, more and edlin now ports

---
+++ CMD/find/find.mk
@@ -0,0 +1,8 @@
+# $Id: makefile,v 1.1 2004/08/16 06:27:05 prokushev Exp $
+
+PORT_NAME = find
+PORT_TYPE = git
+PORT_URL  = https://github.com/FDOS/find
+PORT_REV  = 
+
+!include $(%ROOT)tools/mk/port.mk
