6c613f3af33b440e151072c5157b822469948316
Valery Sedletski <_valerius@mail.ru>
2023-02-28 04:49:51 +0300

    Finish dflatp port.

--- CMD/shared/dflatp/contrib/source/hworld/build_hw.bat
+++
@@ -1,7 +0,0 @@
-REM Set your MAKE tool here:
-SET MAKETOOL=\freedos\borlandc\bin\make
-
-REM REMEMBER! Make TLINK available through path
-PATH %PATH%;\FREEDOS\BORLANDC\BIN
-
-%MAKETOOL% -f hw.mak %1
---
+++ CMD/shared/dflatp/port/dflatp.mk
@@ -0,0 +1,9 @@
+# $Id: makefile,v 1.1 2004/08/16 06:27:05 prokushev Exp $
+
+PORT_NAME = dflatp
+PORT_TYPE = wget
+PORT_URL  = https://ulukai.org/ecm/editsrc/dfp100s.zip
+PORT_REV  = 
+PORT_PATCHES  = dflatp.diff
+
+!include $(%ROOT)tools/mk/port.mk
