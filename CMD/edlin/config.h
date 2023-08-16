682be14dcc82d720a18f42a4eaee6f58fb00d76f
prokushev <yuri.prokushev@gmail.com>
2023-08-16 23:37:06 +0700

    EDLIN updated to version 2.22

--- CMD/edlin/config.h
+++ CMD/edlin/config.h
@@ -1,5 +1,5 @@
 /* config.h.  Generated from config.h.in by configure.  */
-/* config.h.in.  Generated from configure.in by autoheader.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* Define to 1 if you have the `access' function. */
 #define HAVE_ACCESS 1
@@ -16,15 +16,6 @@
 /* Define to 1 if you have the <jctype.h> header file. */
 /* #undef HAVE_JCTYPE_H */
 
-/* Define to 1 if you have the `e' library (-le). */
-/* #undef HAVE_LIBE */
-
-/* Define to 1 if you have the `inks' library (-links). */
-/* #undef HAVE_LIBINKS */
-
-/* Define to 1 if you have the <limits.h> header file. */
-#define HAVE_LIMITS_H 1
-
 /* Define to 1 if you have the `link' function. */
 #define HAVE_LINK 1
 
@@ -38,11 +29,11 @@
 /* Define to 1 if you have the `memmove' function. */
 #define HAVE_MEMMOVE 1
 
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
 /* Define to 1 if you have the `memset' function. */
 #define HAVE_MEMSET 1
+
+/* Define to 1 if you have the <process.h> header file. */
+#define HAVE_PROCESS_H 1
 
 /* Define to 1 if your system has a GNU libc compatible `realloc' function,
    and to 0 otherwise. */
@@ -51,8 +42,14 @@
 /* Define to 1 if you have the `rename' function. */
 #define HAVE_RENAME 1
 
+/* Define to 1 if you have the `stat' function. */
+#define HAVE_STAT 1
+
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
@@ -65,6 +62,12 @@
 
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strpbrk' function. */
+#define HAVE_STRPBRK 1
+
+/* Define to 1 if you have the `strrchr' function. */
+#define HAVE_STRRCHR 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
@@ -82,25 +85,30 @@
 #define PACKAGE "edlin"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT ""
+#define PACKAGE_BUGREPORT "gpietsch@comcast.net"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "edlin"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "edlin 2.10c"
+#define PACKAGE_STRING "edlin 2.22"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "edlin"
 
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "2.10c"
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
 
-/* Define to 1 if you have the ANSI C header files. */
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.22"
+
+/* Define to 1 if all of the C90 standard headers exist (not just the ones
+   required in a freestanding environment). This macro is provided for
+   backward compatibility; new code need not use it. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "2.10c"
+#define VERSION "2.22"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
--- CMD/edlin/edlin.mk
+++ CMD/edlin/edlin.mk
@@ -1,8 +1,10 @@
 # $Id: makefile,v 1.1 2004/08/16 06:27:05 prokushev Exp $
 
+
+
 PORT_NAME = dos$(SEP)edlin
-PORT_TYPE = git
-PORT_URL  = https://github.com/FDOS/edlin
+PORT_TYPE = wget
+PORT_URL  = https://sourceforge.net/projects/freedos-edlin/files/freedos-edlin/2.22/edlin-2.22.zip/download
 PORT_REV  = 
 
 !include $(%ROOT)tools/mk/port.mk
