e888adc7fc267bcb8851a892d10fc50d4547d761
prokushev <yuri.prokushev@gmail.com>
2023-12-09 21:34:21 +0700

    vmdisk template

---
+++ mvdm/main.c
@@ -0,0 +1,6 @@
+#include <mvdm.h>
+
+BOOL VDHOpen(PSZ FileName, PHFILEFileHandle, PULONG ActionTaken, ULONG FileSize, ULONG FileAttribute, ULONG OpenFLag, ULONG OpenMode, PVDH_EOAP EABuf)
+{
+  return FALSE;
+}
---
+++ vmdisk/main.c
@@ -0,0 +1,119 @@
+/* VMDISK is image file creation of FDD disks for boot into MVM. */
+
+#include <os2.h>
+#include <bsedev.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef
+	struct tagIMAGEHEADER
+	{
+		BYTE	Flags;
+		BYTE	Heads;
+		WORD	Tracks;
+		WORD	Sectors;
+		WORD	Bytes;
+	} IMAGEHEADER;
+
+typedef struct tagparmList
+	{
+		UCHAR command;
+		UCHAR drive;
+	} parmList;
+
+/*
+ * error() - Display message, error number and exit program
+*/
+void error(char *msg, int error)
+{
+	fprintf(stderr, "Error %d - %s\n", error, msg);
+	exit(error);
+}
+
+void main(void)
+{
+	HFILE hfDrive;
+	ULONG ulActionTaken;
+	IMAGEHEADER ihImageHeader;
+	BIOSPARAMETERBLOCK bpb;
+	ULONG bpbLength = sizeof(bpb);
+	parmList parm;
+	ULONG parmLength   = sizeof(parm);
+	APIRET rc;      /* return code */
+
+	rc=DosOpen("A:",
+		&hfDrive,
+		&ulActionTaken,
+		0,
+		0,
+		OPEN_ACTION_OPEN_IF_EXISTS,
+		OPEN_FLAGS_DASD | OPEN_FLAGS_NOINHERIT | OPEN_SHARE_DENYNONE,
+		0);
+	if (rc != NO_ERROR)
+		error("DosOpen", rc);
+
+	/*
+	 * Lock the drive
+	*/
+	rc = DosDevIOCtl(
+        	hfDrive,
+	        IOCTL_DISK,                  /* device catagory          */
+	        DSK_LOCKDRIVE,                  /* function code            */
+	        0,                      /* parameter packet address */
+	        0,                      /* parm list length         */
+	        0,                      /* parm list length address */
+	        0,                      /* address of extended BPB  */
+	        0,                      /* length of extended BPB   */
+	        0);                     /* address of ext BPB len   */
+	if (rc != NO_ERROR)
+		error("DosDevIOCtl lock", rc);
+
+	/*
+	 * Get the device parameters
+	 * (BPB = BIOS Parameter Block)
+	*/
+	parm.command = 1;           /* return the BPB for the media
+                                   currently in the drive   */
+	rc = DosDevIOCtl(
+        	hfDrive,
+	        IOCTL_DISK,             /* device catagory          */
+	        DSK_GETDEVICEPARAMS,    /* function code            */
+	        &parm,                  /* parameter packet address */
+	        parmLength,             /* parm list length         */
+	        &parmLength,
+	        &bpb,                /* address of extended BPB  */
+	        bpbLength,           /* length of extended BPB   */
+	        &bpbLength);
+
+	/*
+	 * Unlock the media
+	*/      	
+	rc = DosDevIOCtl(
+		hfDrive,
+		IOCTL_DISK,                  /* device catagory          */
+		DSK_UNLOCKDRIVE,                  /* function code            */
+		0,                      /* parameter packet address */
+	        0,                      /* parm list length         */
+	        0,                      /* parm list length address */
+	        0,                      /* address of extended BPB  */
+	        0,                      /* length of extended BPB   */
+	        0);                     /* address of ext BPB len   */
+	if (rc != NO_ERROR)
+		error("DosDevIOCtl unlock", rc);
+
+      	rc=DosClose(hfDrive);
+	if (rc != NO_ERROR)
+        	error("DosClose", rc);
+
+	ihImageHeader.Flags = 0;
+	ihImageHeader.Heads = (BYTE)bpb.cHeads;
+	ihImageHeader.Tracks = bpb.cSectors / (bpb.usSectorsPerTrack * bpb.cHeads);
+	ihImageHeader.Sectors = bpb.usSectorsPerTrack;
+	ihImageHeader.Bytes = bpb.usBytesPerSector;
+
+	// Write header
+	// Read track by track and write to image file
+
+}
+
