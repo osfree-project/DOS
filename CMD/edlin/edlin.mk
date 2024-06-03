9499e12353ac4292a42fffa015d93053f4a29a7a
prokushev <yuri.prokushev@gmail.com>
2024-06-03 09:07:47 +0700

    edlin updated to 2.24

--- CMD/edlin/edlin.mk
+++ CMD/edlin/edlin.mk
@@ -4,7 +4,7 @@
 
 PORT_NAME = dos$(SEP)edlin
 PORT_TYPE = wget
-PORT_URL  = https://sourceforge.net/projects/freedos-edlin/files/freedos-edlin/2.23/edlin-2.23.zip/download
+PORT_URL  = https://sourceforge.net/projects/freedos-edlin/files/freedos-edlin/2.24/edlin-2.24.zip/download
 PORT_REV  = 
 
 !include $(%ROOT)tools/mk/port.mk
