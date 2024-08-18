de723bc9fe9f76872219ba78821c6abbc8066258
prokushev <yuri.prokushev@gmail.com>
2024-08-18 19:05:11 +0700

    debug updated to 2.50

--- CMD/debug/debug.mk
+++ CMD/debug/debug.mk
@@ -3,7 +3,7 @@
 PORT_NAME = dos$(SEP)debug
 PORT_TYPE = git
 PORT_URL  = https://github.com/Baron-von-Riedesel/DOS-debug
-PORT_REV  = v2.02
+PORT_REV  = v2.50
 PORT_PATCHES  = debug.diff
 
 !include $(%ROOT)tools/mk/port.mk
